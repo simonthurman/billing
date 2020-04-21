6953280393353c2e208726b6694d8b2ce30c26af
Simon Thurman <simonthurman@hotmail.com>
2020-04-21 09:50:17 +0100

    Amend auth

--- Program.cs
+++ Program.cs
@@ -13,24 +13,33 @@ namespace Billing
             WebResponse authRespone = myAuthRequest.GetResponse();
 
 
-            String myurl = "https://management.azure.com/providers/Microsoft.Billing/enrollmentAccounts/83294316/providers/Microsoft.Consumption/usageDetails?api-version=2019-10-01";
-           
-            //String token = CredentialCache.DefaultCredentials;
+            //String myurl = "https://management.azure.com/providers/Microsoft.Billing/enrollmentAccounts/83294316/providers/Microsoft.Consumption/usageDetails?api-version=2019-10-01";
+            String myurl = "https://consumption.azure.com/v3/enrollments/83294316/usagedetails/download?startTime=2020-01-01&endTime=2020-01-02";
+
+
+            Console.WriteLine("Enter token: ");
+            String token = Console.ReadLine();
 
             WebRequest myRequest = WebRequest.Create(myurl);
-            myRequest.Credentials = CredentialCache.DefaultCredentials;
-            //myRequest.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + token);
+            //myRequest.Credentials = CredentialCache.DefaultCredentials;
+            myRequest.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + token);
             myRequest.ContentType ="application/json";
 
 
             WebResponse response = myRequest.GetResponse();
             Console.WriteLine(((HttpWebResponse)response).StatusDescription);
 
+            String path = @"c:\demo\consumption.json";
+
+            //FileStream myStream = File.Create(path);
+            StreamWriter myWriter = new StreamWriter(path);
+
             using (Stream billingStream = response.GetResponseStream())
             {
                 StreamReader reader = new StreamReader(billingStream);
                 string responseFromServer = reader.ReadToEnd();
-                Console.WriteLine(responseFromServer);
+                myWriter.WriteLine(responseFromServer);
+                //Console.WriteLine(responseFromServer);
             }
             Console.WriteLine("Finished");
         }
